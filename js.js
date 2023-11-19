92db6621e8473ca3f71898855d99cebfebb9ab86
Dontlikeouy <86613662+Dontlikeouy@users.noreply.github.com>
2023-11-19 20:26:02 +0300

    new file:   fonts/Montserrat.ttf 	new file:   home.html 	new file:   i/1.jpg 	new file:   i/2.jpg 	new file:   i/3.jpg 	new file:   i/4.jpg 	new file:   i/5.jpg 	new file:   i/Thumbs.db 	new file:   img/Iconic Tv.png 	new file:   img/ear.png 	new file:   img/emty.png 	new file:   img/more.png 	new file:   img/search.png 	new file:   jquery-3.6.3.js 	new file:   js.js 	new file:   style.css

---
+++ js.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function ($) {
+    var slideWidth = $("#slider ul li").width();
+
+
+    $("#button1").click(function () {
+        $("#slider ul").animate(
+            {
+                left: +slideWidth
+            },
+            500,
+            function () {
+                $("#slider ul li:last-child").prependTo("#slider ul");
+                $("#slider ul").css("left", "");
+            }
+        );
+    });
+
+    $("#button2").click(function () {
+        $("#slider ul").animate(
+            {
+                left: -slideWidth
+            },
+            500,
+            function () {
+                $("#slider ul li:first-child").appendTo("#slider ul");
+                $("#slider ul").css("left", "");
+            }
+        );
+    });
+})
